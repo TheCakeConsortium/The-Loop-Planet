6e19e2e3d3f3229078ff94051a510baad3a1b1af
TheCakeConsortium <siowcakee@gmail.com>
2020-10-04 23:17:00 +0800

    added button to go back after winning/losing

---
+++ Assets/Scripts/UIEndScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIEndScene : MonoBehaviour
+{
+    [SerializeField] Button button_goBack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button_goBack.onClick.AddListener(GoBack);
+    }
+
+    void GoBack()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
